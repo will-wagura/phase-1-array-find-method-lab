efb788c635870c758e054b8284566c67b4ec260b
William Ndirangu <wwagura0@gmail.com>
2024-04-04 18:50:51 +0300

    Add

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function isWinner(record) {
+    return record.result === "W"
+}
+function superbowlWin(record) {
+    const win = record.find(isWinner)
+    return win? win.year : undefined
+}
